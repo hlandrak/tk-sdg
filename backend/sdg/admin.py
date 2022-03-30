26809778d4ac06a591d69fe07d1f79cc1290af6b
Håvard Landråk <haavl@stud.ntnu.no>
2022-03-30 13:13:28 +0200

    Fixed import/export with example

--- backend/sdg/admin.py
+++ backend/sdg/admin.py
@@ -5,28 +5,21 @@ from .models import SDG
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
+
+class DocumentResource(resources.ModelResource):
+   class Meta:
+      model = Document
+class DocumentAdmin(ImportExportModelAdmin):
+   resource_class = DocumentResource
+
 # Register your models here.
-"""
-class DocumentResource(resources.ModuleResource):
-    class Meta:
-        module = Document
-        
-
-class DocuemntAdmin2(ImportExportModelAdmin):   
-    resource_class = DocumentResource
-"""
-
-class DocuementAdmin(admin.ModelAdmin):
+"""class DocuementAdmin(admin.ModelAdmin):
     list_display = ('name', 'url','sdgs','sdg_strength','sdg1','sdg2','sdg3','sdg4','sdg5','sdg6','sdg7','sdg8','sdg9','sdg10','sdg11','sdg12','sdg13','sdg14','sdg15','sdg16','sdg17')
-    
-    
+"""
 class SDGAdmin(admin.ModelAdmin):
     list_display =('number', 'description','hex')
 
 
-
-
 #Registrer models here
-admin.site.register(Document,DocuementAdmin)
+admin.site.register(Document, DocumentAdmin)
 admin.site.register(SDG,SDGAdmin)
-#admin.site.register(Document,DocuemntAdmin2)
